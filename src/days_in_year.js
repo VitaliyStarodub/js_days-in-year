701446720c5992227b0250403d052f41b7f01568
Vitalii Starodub <Vitalii.Starodub@playtech.com>
2019-10-18 16:41:42 +0300

    'solution_vitalii'

---
+++ src/days_in_year.js
@@ -0,0 +1,23 @@
+const daysInYear = (year) => {
+    try {
+        if (typeof year === 'number' && /^\+?\d+$/.test(year)) {
+            return isLeapYear(year) ? 366 : 365;
+        }
+    } catch (e) {
+
+    }
+    return 'exception';
+};
+
+
+function isLeapYear(year) {
+    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
+
+
+console.log(daysInYear(2019)); // 365
+console.log(daysInYear('2019')); // exception
+console.log(daysInYear(2020)); // 366
+console.log(daysInYear(2020.2)); // exception
+console.log(daysInYear(2100)); // 365
+console.log(daysInYear({year: 2100})); // exception
